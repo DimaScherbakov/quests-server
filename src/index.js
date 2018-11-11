287065df87affdebb1c9d211586658bbd1ede7d5
dimaScherbakv <dima.r.scherbakov@gmail.com>
2018-11-11 20:04:44 +0200

    add mainPage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+// const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 const app = express()
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send(
-    [{
-      title: "Hello World!",
-      description: "Hi there! How are you?"
-    }]
-  )
+app.get('/quests', function (req, res){
+  res.header('Content-Type','application/json');
+  res.sendFile(path.join(__dirname, '../data/MainPageInfo.json'));
+})
+
+app.get('/quest',function(req,res){
+  console.log(req);
+  // var quest = getQuest(req)
 })
 
 app.listen(8081,
   function(){
-    console.log('Server start on port  ...')})
+    console.log('Server start on port  8081')})
