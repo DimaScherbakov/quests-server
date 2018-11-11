a1b0f69eff0ebb60ddd091789f92a696b4eed478
dimaScherbakv <dima.r.scherbakov@gmail.com>
2018-11-11 10:09:02 +0200

    start version

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send(
+    [{
+      title: "Hello World!",
+      description: "Hi there! How are you?"
+    }]
+  )
+})
+
+app.listen(8081,
+  function(){
+    console.log('Server start on port  ...')})
