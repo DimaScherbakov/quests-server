085d79ad08d8c114592f6b181351d08b4e162847
Dima Scherbakov <dima.r.scherbakov@gmail.com>
2018-11-25 20:18:25 +0200

    add 404 status to the response

--- src/index.js
+++ src/index.js
@@ -9,23 +9,28 @@ const app = express()
 // app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/quests', function (req, res){
-  res.header('Content-Type','application/json');
+app.get('/quests', function (req, res) {
+  res.header('Content-Type', 'application/json');
   res.sendFile(path.join(__dirname, '../data/MainPageInfo.json'));
 })
 
-app.get('/quest/:alias/:globalId',function(req,res){
+app.get('/quest/:alias/:globalId', function (req, res) {
   console.log(req.params);
 
   let allQuests = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/GeneralQuestsInfo.json'), 'utf8'));
-  let respQuest = allQuests.find( (x)=> {
-    return x.alias === req.params.alias && x.globalId.toString() === req.params.globalId} );
-  console.log(respQuest)
-  res.setHeader('Content-Type', 'application/json');
-res.send(JSON.stringify(respQuest));
+  let respQuest = allQuests.find((x) => {
+    return x.alias === req.params.alias && x.globalId.toString() === req.params.globalId
+  });
+  if (respQuest) {
+    // console.log(respQuest)
+    res.setHeader('Content-Type', 'application/json');
+    return res.send(JSON.stringify(respQuest));
+  }
+  res.sendStatus(404);
   // var quest = getQuest(req)
 })
 
 app.listen(8081,
-  function(){
-    console.log('Server start on port  8081')})
+  function () {
+    console.log('Server start on port  8081')
+  })
